c8b777fb07e7bc3b0ab8c73ca7f5547bc0165d29
Kelson Eduardo <kfeduardo2003@gmail.com>
2024-02-23 23:10:58 -0800

    Project initialization

---
+++ src/test/java/br/com/vagalivre/backend/VagaLivreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vagalivre.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagaLivreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
